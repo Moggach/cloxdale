1648098713e426ca1406dfd3fe64dfb16d0c6805
Moggach <acunnane13@gmail.com>
2024-01-03 20:44:12 +0000

    remove un-needed post content type

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,7 +1,6 @@
 import { SchemaTypeDefinition } from 'sanity'
 
 import blockContent from './blockContent'
-import post from './post'
 import credit from './credit'
 import heroSection from './heroSection'
 import contact from './contact'
@@ -9,7 +8,7 @@ import contact from './contact'
 
 
 
-export const schemaTypes = [post, blockContent, credit, heroSection]
+export const schemaTypes = [blockContent, credit, heroSection]
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [post, blockContent, credit, heroSection, contact],
+  types: [blockContent, credit, heroSection, contact],
 }
