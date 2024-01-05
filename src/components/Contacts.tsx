4dcb65010429028d8d4594f9899c5f5735f84d49
Moggach <acunnane13@gmail.com>
2024-01-05 20:27:32 +0000

    add ways to not contact me

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -2,13 +2,22 @@
 
 const Contacts = ({ contacts }) => {
   return (
-    <div id="contact">
-      CONTACT
-      {contacts.map((contact, index) => (
-        <div key={index}>
-        <h3>{contact.title} <a href={contact.link}>here</a></h3>
-        </div>
-      ))}
+    <div id="contact" className="flex flex-col gap-3 md:flex-row md:gap-5">
+
+
+      <div className="flex flex-col gap-3 basis-1/2">
+        CONTACT
+        {contacts.map((contact, index) => (
+          <div key={index}>
+            <h3>{contact.title} <a href={contact.link}>here</a></h3>
+          </div>
+        ))}
+      </div>
+      <div className="flex flex-col gap-3 basis-1/2">
+        WAYS NOT TO CONTACT ME FOR BALANCE
+       
+      </div>
+
 
     </div>
   )
