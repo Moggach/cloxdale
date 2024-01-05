e92afac095cc92f0e86cf9f1de441e24e42f1d56
Moggach <acunnane13@gmail.com>
2024-01-05 19:34:19 +0000

    clicking on name takes you to top off page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,38 +2,54 @@ import Link from 'next/link';
 import React, { useState, useEffect } from 'react';
 
 const Navbar = () => {
- const [isSticky, setSticky] = useState(false)
- const navbarRef = React.createRef<HTMLDivElement>(); 
+  const [isSticky, setSticky] = useState(false);
+  const navbarRef = React.createRef<HTMLDivElement>();
 
- useEffect(() => {
-  const handleScroll = () => {
-    if (navbarRef.current) {
-      setSticky(window.pageYOffset >= navbarRef.current.offsetTop);
-    }
-  };
+  useEffect(() => {
+    const handleScroll = () => {
+      if (navbarRef.current) {
+        setSticky(window.pageYOffset >= navbarRef.current.offsetTop);
+      }
+    };
 
-  window.addEventListener('scroll', handleScroll);
+    window.addEventListener('scroll', handleScroll);
 
-  return () => {
-    window.removeEventListener('scroll', handleScroll);
-  };
- }, [navbarRef]); 
- return (
-  <nav
-    ref={navbarRef}
-    className="z-10 bg-gray-100 h-[100px] sticky top-0 p-4 flex flex-col md:flex-row justify-between md:items-center "
-  >
-      <div> <div className="text-lg font-bold">CAMERON LOXDALE</div>
-      <p>Comedy Writer</p>
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [navbarRef]);
+
+  return (
+    <nav
+      ref={navbarRef}
+      className="z-10 bg-gray-100 h-[100px] sticky top-0 p-4 flex flex-col md:flex-row justify-between md:items-center "
+    >
+      <div>
+        <Link href="#top">
+          <div className="text-lg font-bold">CAMERON LOXDALE</div>
+        </Link>
+        <p>Comedy Writer</p>
       </div>
-   
-    <ul className="flex space-x-4">
-      <li><Link href="#contact"><span className="">Contact</span></Link></li>
-      <li><Link href="#credits"><span className="">Credits</span></Link></li>
-      <li><Link href="#nonsense"><span className="">Nonsense</span></Link></li>
-    </ul>
-  </nav>
- );
+
+      <ul className="flex space-x-4">
+        <li>
+          <Link href="#contact">
+            <div>Contact</div>
+          </Link>
+        </li>
+        <li>
+          <Link href="#credits">
+            <div>Credits</div>
+          </Link>
+        </li>
+        <li>
+          <Link href="#nonsense">
+            <div>Nonsense</div>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
