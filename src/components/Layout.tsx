054cef394c2600f681c6e7da9ee6dafa554e1d88
Moggach <acunnane13@gmail.com>
2024-01-05 20:28:08 +0000

    set max width on main

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({ children }) {
     return (
         <>
             <Header />
-            <div className="p-4 flex flex-col gap-8">{children} </div> 
+            <main className="p-4 flex flex-col gap-8">{children} </main> 
             <Footer />
 
         </>
