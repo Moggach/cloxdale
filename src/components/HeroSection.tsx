99cdecfdbc23ed853637b87f90e6e5f9c1bf51d2
Moggach <acunnane13@gmail.com>
2024-01-05 15:47:14 +0000

    display rich text properly in hero section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,18 +2,29 @@ import Image from 'next/image'
 import { urlForImage } from '~/lib/sanity.image'
 
 const HeroSection = ({ section }) => {
- return (
-   <div className="flex flex-col gap-3">
-     {section[0].excerpt}
-     <Image
-       className=""
-       src={urlForImage(section[0].mainImage).width(500).height(300).url()}
-       height={300}
-       width={500}
-       alt=""
-     />
-   </div>
- )
+  return (
+    <div className="flex flex-col md:flex-row gap-3 md:gap-5">
+      <div className="basis-1/2">
+        {section[0].body.map((block) => (
+          <div key={block._key} style={{ margin: '10px 0' }}>
+            {block.children.map((child) => (
+              <span key={child._key} style={child.marks.includes('strong') ? { fontWeight: 'bold' } : {}}>
+                {child.text}
+              </span>
+            ))}
+          </div>
+        ))}
+      </div>
+
+      <Image
+        className=""
+        src={urlForImage(section[0].image).width(500).height(300).url()}
+        height={300}
+        width={500}
+        alt=""
+      />
+    </div>
+  )
 }
 
 export default HeroSection
