a85c9b50f2dca61f63b28d28ccc95102cfbdf083
Moggach <acunnane13@gmail.com>
2024-01-12 17:25:12 +0000

    add credits slider

--- src/components/Credits.tsx
+++ src/components/Credits.tsx
@@ -2,8 +2,19 @@ import React from 'react';
 import Image from 'next/image';
 import { urlForImage } from '~/lib/sanity.image';
 import Link from 'next/link';
-import 'swiper/swiper-bundle.css';
+
+
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+
 import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
 
 
 const Credits = ({ credit }) => {
@@ -11,30 +22,51 @@ const Credits = ({ credit }) => {
         <div id="credits">
             CREDITS
             <Swiper
-                spaceBetween={20}
-                slidesPerView={2}
+            className="netflix-slider"
+              modules={[Pagination, Scrollbar, A11y]}
+                spaceBetween={30}
+                slidesPerView={2.5}
+                breakpoints={{
+                    320: {
+                        slidesPerView: 2.5,
+                        spaceBetween:30
+                    },
+                    768: {
+                        slidesPerView: 3.5,
+                        spaceBetween: 30
+                    },
+                    1025: {
+                        slidesPerView: 4.5,
+                        spaceBetween: 30
+                    }
+                }}
             >
                 {credit.map((credit, index) => (
                     <SwiperSlide key={index}>
-                        <Link href={credit.externalLink}>
                             <div className="flex flex-col gap-3">
-                                <h3>{credit.title}</h3>
                                 <Image
                                     className=""
-                                    src={urlForImage(credit.image).width(500).height(300).url()}
-                                    height={300}
-                                    width={500}
+                                    src={urlForImage(credit.image).width(600).height(400).url()}
+                                    height={400}
+                                    width={600}
                                     alt=""
                                 />
-                                <p>{credit.excerpt}</p>
+                                 {credit.excerpt.map((block) => (
+                                    <div className="excerpt" key={block._key} style={{ margin: '10px 0' }}>
+                                        {block.children.map((child) => (
+                                            <span key={child._key} style={child.marks.includes('strong') ? { fontWeight: 'bold' } : {}}>
+                                                {child.text}
+                                            </span>
+                                        ))}
+                                    </div>
+                                ))}
+        
                             </div>
-                        </Link>
                     </SwiperSlide>
                 ))}
             </Swiper>
         </div>
-
-    )
+    );
 }
 
 export default Credits;
