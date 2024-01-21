ef0c2b96ff9cc6a2c276a5e1e568e5134b65af1a
Moggach <acunnane13@gmail.com>
2024-01-21 20:05:59 +0000

    add spacing scale

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,15 @@ module.exports = {
       fontFamily: {
         gogh: ['Gogh', 'sans'], 
       },
+      spacing: {
+        '20': '20px',
+        '40': '40px',
+        '60': '60px',
+        '80': '80px',
+        '120': '120px',
+      },
+      gap: (theme) => theme('spacing'), // Use custom spacing values for flex gap
+    },
   },
   plugins: [],
- }
 }
