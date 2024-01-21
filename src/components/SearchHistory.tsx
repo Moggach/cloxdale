7e24d7a091856fb4945b2fadc45ce7776a8a5869
Moggach <acunnane13@gmail.com>
2024-01-21 18:58:02 +0000

    add intersection observer to run search history animation only when scrolled into view

--- src/components/SearchHistory.tsx
+++ src/components/SearchHistory.tsx
@@ -1,46 +1,68 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { motion } from 'framer-motion';
 
 const variants = {
- hidden: { opacity: 0 },
- visible: { opacity: 1 },
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      delayChildren: 0.5,
+      staggerChildren: 1.5, 
+    },
+  },
 };
 
 const SearchHistory = ({ searchItem }) => {
- const [isVisible, setIsVisible] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
+  const containerRef = useRef(null);
 
- useEffect(() => {
-   const timer = setTimeout(() => {
-     setIsVisible(true);
-   }, 1000); // adjust delay as needed
-   return () => clearTimeout(timer);
- }, []);
+  useEffect(() => {
+    const handleIntersection = (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const timer = setTimeout(() => {
+            setIsVisible(true);
+          }, 500);
+          return () => clearTimeout(timer);
+        }
+      });
+    };
 
- return (
-   <>
-     <h3>
-       CHECK OUT CAMERON&apos;S LIVE INTERNET SEARCH HISTORY
-     </h3>
-     <motion.ul
-       initial="hidden"
-       animate={isVisible ? "visible" : "hidden"}
-       variants={{
-         visible: {
-           transition: {
-             delayChildren: 2,
-             staggerChildren: 2,
-           },
-         },
-       }}
-     >
-       {searchItem.map((item, index) => (
-         <motion.li variants={variants} key={index}>
-           {item.text}
-         </motion.li>
-       ))}
-     </motion.ul>
-   </>
- );
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.5, 
+    };
+
+    const observer = new IntersectionObserver(handleIntersection, options);
+
+    if (containerRef.current) {
+      observer.observe(containerRef.current);
+    }
+
+    return () => {
+      if (containerRef.current) {
+        observer.unobserve(containerRef.current);
+      }
+    };
+  }, []);
+
+  return (
+    <div ref={containerRef}>
+      <h3>CHECK OUT CAMERON&apos;S LIVE INTERNET SEARCH HISTORY</h3>
+      <motion.ul
+        initial="hidden"
+        animate={isVisible ? 'visible' : 'hidden'}
+        variants={variants}
+      >
+        {searchItem.map((item, index) => (
+          <motion.li variants={variants} key={index}>
+            {item.text}
+          </motion.li>
+        ))}
+      </motion.ul>
+    </div>
+  );
 };
 
 export default SearchHistory;
