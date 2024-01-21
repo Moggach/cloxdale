4d90e2b931fa114bb7567b11be18581b44abf489
Moggach <acunnane13@gmail.com>
2024-01-21 20:05:31 +0000

    adjust breakpoints on slider

--- src/components/Credits.tsx
+++ src/components/Credits.tsx
@@ -20,22 +20,22 @@ import 'swiper/css/scrollbar';
 const Credits = ({ credit }) => {
     return (
         <div id="credits">
-           <h2 className="font-gogh text-2xl">CREDITS</h2> 
+            <h2 className="font-gogh text-2xl">CREDITS</h2>
             <Swiper
-            className="netflix-slider"
-              modules={[Pagination, Scrollbar, A11y]}
+                className="netflix-slider"
+                modules={[Pagination, Scrollbar, A11y]}
                 spaceBetween={30}
                 slidesPerView={2.5}
                 breakpoints={{
-                    320: {
-                        slidesPerView: 2.5,
-                        spaceBetween:30
-                    },
                     768: {
+                        slidesPerView: 3,
+                        spaceBetween: 30
+                    },
+                    1024: {
                         slidesPerView: 3.5,
                         spaceBetween: 30
                     },
-                    1025: {
+                    1200: {
                         slidesPerView: 4.5,
                         spaceBetween: 30
                     }
@@ -43,25 +43,25 @@ const Credits = ({ credit }) => {
             >
                 {credit.map((credit, index) => (
                     <SwiperSlide key={index}>
-                            <div className="flex flex-col gap-3">
-                                <Image
-                                    className=""
-                                    src={urlForImage(credit.image).width(600).height(400).url()}
-                                    height={400}
-                                    width={600}
-                                    alt=""
-                                />
-                                 {credit.excerpt.map((block) => (
-                                    <div className="excerpt" key={block._key} style={{ margin: '10px 0' }}>
-                                        {block.children.map((child) => (
-                                            <span key={child._key} style={child.marks.includes('strong') ? { fontWeight: 'bold' } : {}}>
-                                                {child.text}
-                                            </span>
-                                        ))}
-                                    </div>
-                                ))}
-        
-                            </div>
+                        <div className="flex flex-col gap-3">
+                            <Image
+                                className=""
+                                src={urlForImage(credit.image).width(600).height(400).url()}
+                                height={400}
+                                width={600}
+                                alt=""
+                            />
+                            {credit.excerpt.map((block) => (
+                                <div className="excerpt" key={block._key} style={{ margin: '10px 0' }}>
+                                    {block.children.map((child) => (
+                                        <span key={child._key} style={child.marks.includes('strong') ? { fontWeight: 'bold' } : {}}>
+                                            {child.text}
+                                        </span>
+                                    ))}
+                                </div>
+                            ))}
+
+                        </div>
                     </SwiperSlide>
                 ))}
             </Swiper>
