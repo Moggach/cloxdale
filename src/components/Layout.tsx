328dcb6ac067030b3aad8cbbbb5c00341fcee800
Moggach <acunnane13@gmail.com>
2024-02-04 19:06:57 +0000

    add dark overlay behind modal

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -11,7 +11,6 @@ import React, { useState } from 'react';
 
 
 export default function Layout({ children }) {
-
     const [isExploding, setIsExploding] = useState(true);
     const [showModal, setShowModal] = useState(true);
 
@@ -21,12 +20,13 @@ export default function Layout({ children }) {
 
     return (
         <>
+            {showModal && <div className="overlay"></div>} 
             <Header />
             <div className={`flex justify-center ${showModal ? 'modal' : ''}`}>
                 {isExploding && <ConfettiExplosion force={0.8} duration={3000} particleCount={250} width={2000} />}
                 {showModal && <Modal onClose={handleCloseModal} />}
             </div>
-            <main className="pt-4 px-4 mx:py-0 flex flex-col gap-60 md:gap-80">{children} </main>
+            <main className="pt-4 px-4 mx:py-0 flex flex-col gap-60 md:gap-80">{children}</main>
             <Footer />
         </>
     )
