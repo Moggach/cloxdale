0f4e0f8c02f1e8f23ace360fc459f33ecd5e0e75
Moggach <acunnane13@gmail.com>
2024-02-17 17:43:23 +0000

    add hover states to nav links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,23 +20,23 @@ const Navbar = () => {
   return (
     <nav
       ref={navbarRef} 
-      className="z-10 bg-neonGreen sticky top-0 p-20 lg:px-60 flex gap-20 flex-col md:flex-row justify-between md:items-center mb-60"
+      className="z-10 bg-neonGreen sticky top-0 p-20 lg:px-60 flex gap-20 flex-col md:flex-row justify-between md:items-center mb-40 lg:mb-60"
     >
       <div className="flex gap-20 flex-col">
-        <h1 className="font-gogh text-xl cursor-pointer">
+        <h1 className="font-gogh text-xl cursor-pointer hover:text-white">
           <SmoothScrollLink toTop={true}>CAMERON LOXDALE</SmoothScrollLink>
         </h1>
         <p className="font-gogh text-lg">Comedy Writer</p>
       </div>
 
       <ul className="flex flex-wrap font-gogh gap-x-[10px] uppercase text-lg">
-        <li>
+        <li className='hover:text-white'>
           <SmoothScrollLink to="#contacts" offset={offset}>Contact</SmoothScrollLink>
         </li>
-        <li>
+        <li className='hover:text-white'>
           <SmoothScrollLink to="#credits" offset={offset}>Credits</SmoothScrollLink>
         </li>
-        <li>
+        <li className='hover:text-white'>
           <SmoothScrollLink to="#nonsense" offset={offset}>Nonsense</SmoothScrollLink>
         </li>
       </ul>
