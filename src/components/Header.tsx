f0914cfbc7bfda78db9320e22e9a950f5e8cf6c2
Moggach <acunnane13@gmail.com>
2024-06-02 18:56:12 +0100

    set max-width on title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -50,7 +50,7 @@ const Header = forwardRef((props, ref) => {
       {theme === 'tooDark' && <div className="black-overlay"></div>}
       <nav
         ref={combinedRef}
-        className={`z-10 sticky top-0 p-20 lg:px-60 flex gap-20 flex-col md:flex-row justify-between md:items-center ${textColor} ${bgColor}`}
+        className={`z-10 sticky top-0 p-20 xl:px-60 flex gap-20 flex-col md:flex-row justify-between md:items-center ${textColor} ${bgColor}`}
       >
         <div className="flex gap-20 flex-col">
           <h1 className="font-gogh text-xl cursor-pointer uppercase max-w-lg">
