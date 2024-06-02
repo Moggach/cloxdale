944a86e6dca998d4cf5f7f4af7f4d5374f4d83a5
Moggach <acunnane13@gmail.com>
2024-06-02 19:10:09 +0100

    add searchItems array

--- src/components/SearchHistory.tsx
+++ src/components/SearchHistory.tsx
@@ -13,7 +13,7 @@ const variants = {
   },
 };
 
-const SearchHistory = ({ searchItem }) => {
+const SearchHistory = () => {
   const [isVisible, setIsVisible] = useState(false);
   const containerRef = useRef(null);
 
@@ -54,6 +54,25 @@ const SearchHistory = ({ searchItem }) => {
   const bgColor = theme === 'dark' ? 'bg-darkPrimary' : 'bg-lightPrimary';
   const textColor = theme === 'dark' ? 'text-darkText' : 'text-lightText';
 
+  const searchItems = [
+    { text: 'Weird bumps on back'},
+    { text: 'Bumps grown into small fingers?' },
+    { text: 'Hand emerging from back webMD' },
+    { text: 'Can a man grow a third hand?' },
+    { text: 'Is that what backhand means?' },
+    { text: 'Why is backhand just doing jazz hands?' },
+    { text: 'Backhand has just passed a typing exam??' },
+    { text: 'Solid gold glove, singular, next day delivery' },
+    { text: 'How to change thumbprint recognition' },
+    { text: 'Can a hand steal a man&apos;s identity?' },
+    { text: 'Bank account saying it&apos;s empty' },
+    { text: 'Backhand wearing several expensive rings over solid gold glove' }
+
+
+
+
+  ];
+
   return (
     <div ref={containerRef} className="">
       <h2 className={`font-gogh text-lg mb-[30px] p-3 rounded-sm -rotate-3 inline-block ${bgColor} ${textColor}`}>CHECK OUT CAMERON&apos;S LIVE INTERNET SEARCH HISTORY!</h2>
@@ -63,9 +82,9 @@ const SearchHistory = ({ searchItem }) => {
         animate={isVisible ? 'visible' : 'hidden'}
         variants={variants}
       >
-        {searchItem.map((item, index) => (
+        {searchItems.map((item, index) => (
           <motion.li variants={variants} key={index}>
-           <div className='font-karla  text-base'>{item.text}</div> 
+           <div className='font-karla text-base'>{item.text}</div> 
           </motion.li>
         ))}
       </motion.ul>
