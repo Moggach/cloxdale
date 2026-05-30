2b5e6c32a0a2f900a0b6977fb65db54e5fa7362a
Moggach <acunnane13@gmail.com>
2026-05-30 20:37:34 +0100

    justify text center in hero section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -7,7 +7,7 @@ const HeroSection = ({ section }) => {
   return (
     <>
     <div className="flex flex-col-reverse lg:flex-row gap-40 font-karla text-base ">
-      <div className="basis-1/2">
+      <div className="basis-1/2 text-center">
         {section[0].body.map((block) => {
           return (
             <div key={block._key} style={{ margin: '10px 0' }}>
@@ -29,7 +29,7 @@ const HeroSection = ({ section }) => {
         layout="responsive"
         alt=""
       />
-      <figcaption className="pt-2 font-karla text-md italic opacity-60 border-l-2 border-lightPrimary pl-3 mt-2">A known mover and shaker in the industry, Cameron will typically wear shades to intimidate his opposite number in a general meeting.</figcaption>
+      <figcaption className="pt-2 text-center font-karla text-md italic opacity-60 border-l-2 border-lightPrimary pl-3 mt-2">A known mover and shaker in the industry, Cameron will typically wear shades to intimidate his opposite number in a general meeting.</figcaption>
       </div>
       </div>
 <Dots/>
