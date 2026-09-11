cbe05ec3b4618ed9de863650e876bcceaa25813f
Moggach <acunnane13@gmail.com>
2026-09-11 19:52:20 +0100

    styling updates to insta page

--- src/pages/instagram.tsx
+++ src/pages/instagram.tsx
@@ -13,14 +13,14 @@ function InstagramContent() {
 
   return (
     <div className="flex flex-col gap-20 font-fira text-base max-w-2xl lg:max-w-6xl">
-      <p>Cameron Loxdale does not have Instagram because he is better than you.</p>
+      <p className="text-xl font-semibold">Cameron Loxdale does not have Instagram because he is better than you.</p>
 
       <Image
         src="/logos/war_and_peace.jpg"
         width={4284}
         height={5712}
         alt="Cameron Loxdale thoughtfully reading War and Peace instead of scrolling Instagram"
-        className="w-full h-auto rounded-md max-w-sm"
+        className="w-full h-auto rounded-md max-w-2xl lg:max-w-4xl mx-auto"
         style={imageShadow}
       />
     </div>
